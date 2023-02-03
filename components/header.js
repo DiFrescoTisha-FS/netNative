084e72273817f0763a12f0045ded6db5de79a220
Tisha Di Fresco <tbdifresco@student.fullsail.edu>
2023-02-02 22:08:39 -0500

    init

---
+++ components/header.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Header() {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.title}>My Todos</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        height: 80,
+        paddingTop: 38,
+        backgroundColor: '#e1affd'
+    },
+    title: {
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    }
+})
